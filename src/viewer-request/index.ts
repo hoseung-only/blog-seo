563da8fb7b58ac19bb9a350136510a959617bb16
HoseungJang <hsjang.dev@gmail.com>
2021-11-14 02:57:55 +0900

    fix viewer-request

--- src/viewer-request/index.ts
+++ src/viewer-request/index.ts
@@ -1,28 +1,13 @@
-import {
-  CloudFrontRequest,
-  CloudFrontRequestEvent,
-  CloudFrontResultResponse,
-} from "aws-lambda";
+import { CloudFrontRequest, CloudFrontRequestEvent } from "aws-lambda";
 
 import { Headers } from "../helpers/Headers";
 
-const Regex = {
-  UUID: /\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b/,
-};
-
-const defaultOgImage =
-  "https://blog-media.hoseung.me/blog-og-main-profile-image.png";
-
-export async function handler(
-  event: CloudFrontRequestEvent
-): Promise<CloudFrontRequest | CloudFrontResultResponse> {
+export async function handler(event: CloudFrontRequestEvent): Promise<CloudFrontRequest> {
   const request = event.Records[0].cf.request;
   const headers = new Headers(request.headers);
 
   const headerName = "X-Viewer-User-Agent";
-  request.headers[headerName.toLowerCase()] = [
-    { key: headerName, value: headers.get("User-Agent") },
-  ];
+  request.headers[headerName.toLowerCase()] = [{ key: headerName, value: headers.get("User-Agent") }];
 
   return request;
 }
